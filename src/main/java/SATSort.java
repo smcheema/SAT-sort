17e3f3c2e5014b9ad89a2bd9261e285795b7fc73
smcheema <71226369+smcheema@users.noreply.github.com>
2021-07-23 16:18:51 -0400

    [DuplicateEntryFix] Re-work sorting algorithm to be more concise & accomodate duplicates. (#1)
    * Re-work sorting algorithm to accomodate duplicates.
    
    * Make proof more intuitive.
    
    * Re-work testcase.
    
    Co-authored-by: Saad Cheema <saadchee@amazon.com>

--- src/main/java/SATSort.java
+++ src/main/java/SATSort.java
@@ -3,41 +3,51 @@ import com.google.ortools.sat.CpModel;
 import com.google.ortools.sat.CpSolver;
 import com.google.ortools.sat.CpSolverStatus;
 import com.google.ortools.sat.IntVar;
-import com.google.ortools.util.Domain;
+import com.google.ortools.sat.LinearExpr;
 
-import java.util.Arrays;
 
 final public class SATSort {
 
     protected int[] sortIt(int[] arrayToSort) {
-        // load native Java jars, OR-Tools have a set-up process to link the Java abstraction 
-        // and the underlying C++ [https://developers.google.com/optimization/install/java]
+        // load native Java jars.
         Loader.loadNativeLibraries();
         final CpModel model = new CpModel();
-        final long[] domain = Arrays.stream(arrayToSort)
-                .mapToLong(i -> i)
-                .toArray();
-        final IntVar[] slots = new IntVar[arrayToSort.length];
+        // Different approach this time : we model indices.
+        // i.e, I tell CP-SAT : each variable in this set ranges :
+        // from 0..arr.len, and is unique.
+        // Intuitively, for any array[], our indices represent 1-to-1 where an element at the same index ends up
+        // in the sorted array. For example, for [1, 5, 4, 1, 8], a feasible (I say feasible since duplicate entries make multiple assignments possible)
+        // would be [0, 3, 2, 1, 4]. i.e, arr[0] == 1 ends up at index 0 in the sorted array,
+        // arr[1] == 5 ends up at index 3, and so on.
+        final IntVar[] indices = new IntVar[arrayToSort.length];
         for (int index = 0; index < arrayToSort.length; index++) {
             // Constrain our sorted slots to contain integers present in the input array.
-            slots[index] = model.newIntVarFromDomain(Domain.fromValues(domain), "Slot: " + index);
-            // Define the ordering constraint. Surprisingly easy to make this happen using OR-tools, the
-            // manual clauses I wrote following the Sudoku guide weren't environment friendly to say the least :P
-            if (index > 0) model.addGreaterOrEqual(slots[index], slots[index - 1]);
+            indices[index] = model.newIntVar(0,  arrayToSort.length - 1, "Index: " + index);
         }
-        // Constrain each slot to hold a different integer. This is really not what we want, but I can't figure out
-        // how else to go about this. Biggest shortcoming of this approach is that we can't sort arrays with duplicates
-        // using this. Have a look at :
-        // shorturl.at/nptJ3
-        // Hopefully once I build more comfort with this sort of problem solving I'll see the error in my ways.
-        model.addAllDifferent(slots);
+        model.addAllDifferent(indices);
+
+        // this is really neat and is where the magic happens. One way of thinking about sorted arrays is :
+        // I have indices, and elements. Maximize for me the scalar product of my indices and elements.
+        // i.e, put my largest elements at my largest indices.
+        // Small proof by contradiction : suppose the above is false, and our array is ordered, this
+        // implies that for all i in the set {0..arr.len - 1 - 1}, arr[i + 1] >= arr[i] AND there exist
+        // at-least one pair of numbers that can be swapped to achieve a higher sum. Since indices are non-negative
+        // entities, the only way it's possible for swap(arr[i], arr[j]) to yield a higher scalar product is iff
+        // the element at the lower index was larger than the element at the higher index, thus contradicting our
+        // ordering assumption.
+        // Pretty neat way of thinking about sorting.
+        model.maximize(LinearExpr.scalProd(indices, arrayToSort));
         CpSolver solver = new CpSolver();
         CpSolverStatus status = solver.solve(model);
         if (status == CpSolverStatus.FEASIBLE || status == CpSolverStatus.OPTIMAL) {
             int[] retVal = new int[arrayToSort.length];
             for (int index = 0; index < arrayToSort.length; index++) {
-                // Read SATisfiable values from the solver.
-                retVal[index] = (int) solver.value(slots[index]);
+                // to explain this, consider the above example :
+                // arr = [1, 5, 4, 1, 8], solution = [0, 3, 2, 1, 4]
+                // so, go over the index arr, and each value tells us where the corresponding element
+                // in the unsorted array should end up. So 1 ends up at index 0.
+                // 5 ends up at index 3, so on.
+                retVal[(int) solver.value(indices[index])] = arrayToSort[index];
             }
             return retVal;
         } else {
